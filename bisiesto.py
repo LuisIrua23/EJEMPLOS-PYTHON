fee7e0f70416e3168a8eec64ff302bf5ece1bf07
LuisIrua23 <lairua@soy.sena.edu.co>
2025-03-14 10:52:44 -0500

    LUIS

--- bisiesto.py
+++ bisiesto.py
@@ -2,4 +2,5 @@ año = int(input("digite el año: "))
 if año % 4 == 0 and año % 100 != 0 or año % 400 == 0:
     print("El año es bisiesto")
 else:
-    print("El año no es bisiesto")           
+    print("El año no es bisiesto")     
+          
