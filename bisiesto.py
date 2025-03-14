9ccf0152cc22400693b1effc4c475b32c63e3451
LuisIrua23 <lairua@soy.sena.edu.co>
2025-03-14 11:15:11 -0500

    luis

--- bisiesto.py
+++ bisiesto.py
@@ -3,4 +3,5 @@ if año % 4 == 0 and año % 100 != 0 or año % 400 == 0:
     print("El año es bisiesto")
 else:
     print("El año no es bisiesto")     
-    
+# What is the output of this code?
+# a) El año es bisiesto
