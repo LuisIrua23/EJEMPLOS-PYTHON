80df826228e59cac1a0a3a552b83eef9ee266388
LuisIrua23 <lairua@soy.sena.edu.co>
2025-03-17 18:28:18 -0500

    LUIS

---
+++   /TALLER1/5.suma-res-mul-divi.py
@@ -0,0 +1,13 @@
+import random
+numero = random.randint(10, 99)
+print("el numero generado es: ", numero)
+b=numero//10  
+c=numero%10   
+suma=b+c
+resta=b-c
+multiplicacion=b*c
+division=b/c
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicacion es: ", multiplicacion)
+print("La division es: ", division)
