71fca25497d76a2951109472bac7c93f7776b6ac
LuisIrua23 <lairua@soy.sena.edu.co>
2025-03-17 00:13:23 -0500

    luis

--- numeroprimos.py
+++ numeroprimos.py
@@ -1,19 +1,34 @@
-def is_prime(num):
-    if num <= 1:
+print("numeros primos")
+#numetros divisibles por 1 y por si mismos
+#entero y positivo y mayor que 1
+#ejemplo 2,3,5,7,11,13,17,19
+#6
+# 6 % 2 = 0
+# 6 % 3 = 0
+# 6 % 4 = 2
+# 6 % 5 = 1
+# 6 No es numero primo
+#7
+# 7 % 2 = 1
+# 7 % 3 = 1
+# 7 % 4 = 3
+# 7 % 5 = 2
+# 7 % 6 = 1
+# 7  es numero primo
+#num = int(input("ingrese un numero: "))
+
+
+def es_primo(numero):
+    if numero < 2:
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
+    for i in range(2,numero):
+        if numero % i == 0:
             return False
     return True
 
-def prime_numbers_in_range(start, end):
-    primes = []
-    for num in range(start, end + 1):
-        if is_prime(num):
-            primes.append(num)
-    return primes
-
-# Example usage
-start = 10
-end = 50
-print(f"Prime numbers between {start} and {end}: {prime_numbers_in_range(start, end)}")
+numero = int(input("ingrese un numero: "))
+#for numero in range(2,10):
+if es_primo(numero):
+        print(numero , "es primo: ")
+else:
+        print(numero , "no es primo: ")
